cdee814e0f88c9124176f30cc3e7bb4ae114da71
Tom Andrews <git@tomandrews.co.uk>
2010-10-18 23:58:22 +0100

    Added command line input, but not correctly calculating where we are in the list of letters.

--- twitter-username.rb
+++ twitter-username.rb
@@ -1,43 +1,64 @@
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
+require 'optparse'
+#include CommandLine
+
+options = {}
+optparse = OptionParser.new do |opts|
+  options[:index] = 0
+  opts.on( '-i', '--index idx', 'Start index of search where 0 is a, 1 is b and so fourth') do |idx|
+    options[:index] = idx.to_i
+  end
+end
+optparse.parse!
+
+i = options[:index]
 
 letters = Array.new
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 found = false
-i = 0
 # Zero based index
 length = letters.length - 1
 
+break_point = 0
+no_of_letters = 0
 
 while !found
-	poss_name = ''
+  puts 'Index: ' + i.to_s
+  puts 'No of letters ' + no_of_letters.to_s
+  puts 'Break point ' + break_point.to_s
+	twitter_name = ''
 
+	if (i >= break_point)
+	  while (length ** no_of_letters) < i
+		  no_of_letters = no_of_letters + 1
+	  end
+	  break_point = i + (length ** no_of_letters)
+  end
 
-	no_of_letters = 1
-	while (length ** no_of_letters) < i
-		no_of_letters = no_of_letters + 1
-	end
-
-	temp = i
-	while no_of_letters >= 1
-		if (temp < (length ** no_of_letters))
-			current_letter += letters[temp % length]
-		else
-		end
-
-		no_of_letters = no_of_letters - 1
+	carry = i
+	twitter_name = ''
+	no_of_letters_tmp = no_of_letters
+	while no_of_letters_tmp >= 0
+	  max = length ** no_of_letters_tmp
+	  current_letter_idx = carry % (max)
+	  puts 'current_letter_idx ' + current_letter_idx.to_s
+	  carry = carry % (max)
+	  current_letter = letters[current_letter_idx]
+	  twitter_name += current_letter
+	  no_of_letters_tmp = no_of_letters_tmp - 1
 	end
 
 	begin
-		puts 'Trying: ' + poss_name
-		open('http://twiiter.com/' + poss_name)
+		puts 'Trying: ' + twitter_name
+		open('http://twitter.com/' + twitter_name)
 	rescue
-		break
+		break if twitter_name != 'i'
 	else
 	end
 	i = i + 1
 end
 
-puts 'Found: ' + poss_name
+puts 'Found: ' + twitter_name
